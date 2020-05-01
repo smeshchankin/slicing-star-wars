e09c08357ac36b4eacf607546b38973237518829
Sergey Meshchankin <smeshchankin@gmail.com>
2020-05-01 20:10:46 +0300

    [SW-007] Landing / episodes from js array

--- js/main.js
+++ js/main.js
@@ -5,24 +5,35 @@
         { id: 6, img: '06.jpg', episode: 'VI', name: 'Return of the Jedi', year: 1983 },
 
         { id: 1, img: '01.jpg', episode: 'I', name: 'The Phantom Menace', year: 1999 },
-        { id: 2, img: '02.jpg', episode: 'II', name: 'Attack of the Clones', year: 2002 },
-        { id: 3, img: '03.jpg', episode: 'III', name: 'Revenge of the Sith', year: 2005 },
+        { id: 2, img: '00.jpg', episode: 'II', name: 'Attack of the Clones', year: 2002 },
+        { id: 3, img: '00.jpg', episode: 'III', name: 'Revenge of the Sith', year: 2005 },
 
-        { id: 7, img: '07.jpg', episode: 'VII', name: 'The Force Awakens', year: 2015 },
-        { id: 8, img: '08.jpg', episode: 'VIII', name: 'The Last Jedi', year: 2017 },
-        { id: 9, img: '09.jpg', episode: 'IX', name: 'The Rise of Skywalker', year: 2019 }
+        { id: 7, img: '00.jpg', episode: 'VII', name: 'The Force Awakens', year: 2015 },
+        { id: 8, img: '00.jpg', episode: 'VIII', name: 'The Last Jedi', year: 2017 },
+        { id: 9, img: '00.jpg', episode: 'IX', name: 'The Rise of Skywalker', year: 2019 }
     ];
 
     const episodesElem = document.querySelector('#episodes');
-    const episodeElem = document.querySelector('.episode');
+    const templateElem = episodesElem.querySelector('.episode');
+    episodesElem.removeChild(templateElem);
 
-    episode = episodes[0];
-    if (episode.img) {
-        episodeElem.innerHTML = episodeElem.innerHTML.replace('00.jpg', episode.img);
+    if (episodes) {
+        for (let i = 0; i < episodes.length; i++) {
+            let node = templateElem.cloneNode(true);
+            episodesElem.appendChild(fillNode(node, episodes[i]));
+        }
     }
 
-    Object.keys(episode).forEach(function(key) {
-        let value = episode[key];
-        episodeElem.innerHTML = episodeElem.innerHTML.replace('{{' + key + '}}', value);
-    });
+    function fillNode(node, data) {
+        if (data.img) {
+            node.innerHTML = node.innerHTML.replace('00.jpg', data.img);
+        }
+
+        Object.keys(data).forEach(function(key) {
+            let value = data[key];
+            node.innerHTML = node.innerHTML.replace('{{' + key + '}}', value);
+        });
+
+        return node;
+    }
 }());
