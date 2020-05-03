5bf77148ed161e8f2003d52bceb22c52a6b974c4
Sergey Meshchankin <smeshchankin@gmail.com>
2020-05-03 21:11:18 +0300

    [SW-006] Landing / episodes carousel

--- js/main.js
+++ js/main.js
@@ -24,6 +24,25 @@
         }
     }
 
+    $(document).ready(function() {
+        $('.owl-carousel').owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 3
+                },
+                1000: {
+                    items: 4
+                }
+            }
+        })
+    });
+
     function fillNode(node, data) {
         if (data.img) {
             node.innerHTML = node.innerHTML.replace('00.jpg', data.img);
